b6cf13731ef1148d01bd863d105b395358e21906
DV-13 <gjhneufkmcrbqabkbgg@gmail.com>
2016-12-04 18:50:46 +0300

    Выделил копирование БД в отдельный класс.

--- Infotable/MainActivity.cs
+++ Infotable/MainActivity.cs
@@ -12,24 +12,10 @@ namespace Infotable
 		{
 			base.OnCreate(bundle);
 
+			//Скопировать БД на устройсво
 			string dbName = "testtimetableworks.sqlite";
-			string dbPath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), dbName);
-			// Check if your DB has already been extracted.
-			if (!File.Exists(dbPath))
-			{
-				using (BinaryReader br = new BinaryReader(Assets.Open(dbName)))
-				{
-					using (BinaryWriter bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
-					{
-						byte[] buffer = new byte[2048];
-						int len = 0;
-						while ((len = br.Read(buffer, 0, buffer.Length)) > 0)
-						{
-							bw.Write(buffer, 0, len);
-						}
-					}
-				}
-			}
+			BinaryReader br = new BinaryReader(Assets.Open(dbName));
+			copyDB.copy(br, dbName);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
---
+++ Infotable/copyDB.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+
+namespace Infotable
+{
+	class copyDB : Activity
+	{
+		public static void copy(BinaryReader br, string dbName)
+		{
+			//string dbName = "testtimetableworks.sqlite";
+			string dbPath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), dbName);
+			// Check if your DB has already been extracted.
+			if (!File.Exists(dbPath))
+			{
+				using (br/*BinaryReader br = new BinaryReader(Assets.Open(dbName))*/)
+				{
+					using (BinaryWriter bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+					{
+						byte[] buffer = new byte[2048];
+						int len = 0;
+						while ((len = br.Read(buffer, 0, buffer.Length)) > 0)
+						{
+							bw.Write(buffer, 0, len);
+						}
+					}
+				}
+			}
+		}
+	}
+}
