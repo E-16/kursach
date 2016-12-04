bb772f275df3b39988c4aa215e6a23160eee74ca
DV-13 <gjhneufkmcrbqabkbgg@gmail.com>
2016-12-04 18:14:09 +0300

    Копирование БД на устройство

--- Infotable/MainActivity.cs
+++ Infotable/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.IO;
 
 namespace Infotable
 {
@@ -10,6 +11,25 @@ namespace Infotable
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
+
+			string dbName = "testtimetableworks.sqlite";
+			string dbPath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), dbName);
+			// Check if your DB has already been extracted.
+			if (!File.Exists(dbPath))
+			{
+				using (BinaryReader br = new BinaryReader(Assets.Open(dbName)))
+				{
+					using (BinaryWriter bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+					{
+						byte[] buffer = new byte[2048];
+						int len = 0;
+						while ((len = br.Read(buffer, 0, buffer.Length)) > 0)
+						{
+							bw.Write(buffer, 0, len);
+						}
+					}
+				}
+			}
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
