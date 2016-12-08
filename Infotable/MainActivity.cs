546ef09f5985a444ff39181dee8e91c58ded69d6
DV-13 <gjhneufkmcrbqabkbgg@gmail.com>
2016-12-08 13:37:53 +0300

    Создал тему, заменил тулбар на кастомный.

--- Infotable/MainActivity.cs
+++ Infotable/MainActivity.cs
@@ -19,6 +19,11 @@ namespace Infotable
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+
+			//toolbar
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetActionBar(toolbar);
+			ActionBar.Title = "My Toolbar";
 		}
 	}
 }
--- Infotable/Resources/Resource.Designer.cs
+++ Infotable/Resources/Resource.Designer.cs
@@ -60,17 +60,20 @@ namespace Infotable
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int tableLayout1 = 2131034112;
+			// aapt resource value: 0x7f060001
+			public const int tableLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f050001
-			public const int tableRow1 = 2131034113;
+			// aapt resource value: 0x7f060002
+			public const int tableRow1 = 2131099650;
 			
-			// aapt resource value: 0x7f050002
-			public const int tableRow2 = 2131034114;
+			// aapt resource value: 0x7f060003
+			public const int tableRow2 = 2131099651;
 			
-			// aapt resource value: 0x7f050003
-			public const int tableRow3 = 2131034115;
+			// aapt resource value: 0x7f060004
+			public const int tableRow3 = 2131099652;
+			
+			// aapt resource value: 0x7f060000
+			public const int toolbar = 2131099648;
 			
 			static Id()
 			{
@@ -87,6 +90,9 @@ namespace Infotable
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int toolbar = 2130903041;
 			
 			static Layout()
 			{
@@ -113,6 +119,22 @@ namespace Infotable
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int MyTheme = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
